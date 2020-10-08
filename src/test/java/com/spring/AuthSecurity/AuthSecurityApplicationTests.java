58c70e8a53c2bf5f8cc6432c1190e06a0c8f96a4
backup <backup@backup-pc>
2020-10-08 20:06:54 +0530

    Security OAUTH2

---
+++ src/test/java/com/spring/AuthSecurity/AuthSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.AuthSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
